552ee52f81a65aa12dd3b8417cad5619995a7eaa
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-11-03 13:33:21 -0300

    Gulp test cp files

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,9 +1,31 @@
 const gulp = require('gulp')
 const series = gulp.series
 
-function copiar(cb) {
-    console.log('Tarefa de copiar!')
+function antes1(cb) {
+    console.log('Tarefa antes 1!')
+    return cb()
+}
+function antes2(cb) {
+    console.log('Tarefa antes 2!')
     return cb()
 }
 
-module.exports.default = series(copiar)
+function copiar(cb) {
+    // gulp.src(['pastaA/arquivo1..txt', pastaB])
+
+    gulp.src('pastaA/**/*.txt')
+        // .pipe(imagePelaMetade())
+        // .pipe(imagePretoEBranco())
+        // .pipe(transformacaoA())
+        // .pipe(transformacaoB())
+        // .pipe(transformacaoC())
+        .pipe(gulp.dest('pastaB'))    
+    return cb()
+}
+
+function fim(cb) {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+
+module.exports.default = series(antes1,antes2,copiar,fim)
